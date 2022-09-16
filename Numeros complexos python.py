2678b3fc165db8f67158e29690f3b1f2910b1e8b
Wagner de Farias Soares <wagnerdefarias12@gmail.com>
2022-09-16 20:39:43 -0300

    Create Numeros complexos python.py

---
+++ Numeros complexos python.py
@@ -0,0 +1,22 @@
+from typing_extensions import Self
+
+
+class complexos:
+
+def __init__ (self, a, b ,c)
+
+self.a = 2 - 5j
+self.b = complex (4 + 2)
+self.c = 8 - 3j
+
+print (a + b)
+print (b / c)
+print (a * c)
+print (c - a)
+
+print (a.real)
+print (a.imag)
+print (b.real)
+print (b.imag)
+print (c.real)
+print (c.imag)
